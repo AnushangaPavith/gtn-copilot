a7446d3fdf54a216dd40518c6a29aee1d9c97e78
Anushanga Pavith <p.anushanga@gtngroup.com>
2023-10-29 12:31:06 +0530

    Added API call services

---
+++ src/services/services.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_BASE_URL = "http://localhost:5000/random_response";
+
+class Service {
+    getResponse(message) {
+        console.log(message);
+        return axios.post(API_BASE_URL, { message });
+    }
+}
+
+export default new Service();

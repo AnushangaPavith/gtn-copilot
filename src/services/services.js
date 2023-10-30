aa5e720ff0d707bdbe9d66adf7e243ca2d7de13e
ChandimaA <a.chandima@gtngroup.com>
2023-10-30 17:13:29 +0530

    changes

--- src/services/services.js
+++ src/services/services.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://127.0.0.1:8000/info/invoke";
+const API_BASE_URL = "http://127.0.0.1:8000/dynamic-data-retrieve/invoke";
 
 class Service {
     getResponse(input) {
